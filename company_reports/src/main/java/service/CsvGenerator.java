d1c80a891ca804562c80175429349f455865861b
Priyankha <priyankharajkumar742001@gmail.com>
2024-05-12 20:13:44 +0530

    project updated with count of companies

--- company_reports/src/main/java/service/CsvGenerator.java
+++ company_reports/src/main/java/service/CsvGenerator.java
@@ -16,11 +16,9 @@
 //                writer.println(company.getId()+"," +company.getDate() +","+company.getName() + ", " + company.getTotalRevenue()+ ", " + company.getProfitPercentage());
 //            }
 //
-//            // Write sums
 //            writer.println("Total Revenue, " + sums.get("totalRevenue"));
 //            writer.println("Total Profit Percentage, " + sums.get("profitPercentage"));
 //
-//            // Flush and close writer
 //            writer.flush();
 //            writer.close();
 //        }
@@ -38,23 +36,22 @@ import java.util.Map;
 
 public class CsvGenerator {
     public static void generateCsv(List<Company> companies, Map<String, Double> sums, PrintWriter writer) throws IOException {
-        // Write CSV header
         writer.println("Id,Date, Company Name, TotalRevenue, Profit Percentage");
-
         double totalRevenue = 0.0;
         double totalProfitPercentage = 0.0;
+        int count = companies.size();
+
 
         for (Company company : companies) {
             writer.println(company.getId() + "," + company.getDate() + "," + company.getName() + ", " + company.getTotalRevenue() + ", " + company.getProfitPercentage());
             totalRevenue += company.getTotalRevenue();
             totalProfitPercentage += company.getProfitPercentage();
-        }
 
-        // Write sums
+        }
         writer.println("Total Revenue, " + totalRevenue);
         writer.println("Total Profit Percentage, " + totalProfitPercentage);
+        writer.println("Total Records, " + count);
 
-        // Flush and close writer
         writer.flush();
         writer.close();
     }
